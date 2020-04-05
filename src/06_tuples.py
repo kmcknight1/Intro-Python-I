3b4b6466c8e5f6060cbcecc743385417c8fec5f5
kmcknight1 <katiemcknight1991@gmail.com>
2020-04-05 15:14:16 -0500

    completes 06 challenge

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -17,12 +17,14 @@ automatically.
 
 import math
 
+
 def dist(a, b):
     """Compute the distance between two x,y points."""
     x0, y0 = a  # Destructuring assignment
     x1, y1 = b
 
     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+
 
 a = (2, 7)   # <-- x,y coordinates stored in tuples
 b = (-14, 72)
@@ -31,14 +33,19 @@ b = (-14, 72)
 print("Distance is: {:.2f}".format(dist(a, b)))
 
 
-
 # Write a function `print_tuple` that prints all the values in a tuple
 
-# YOUR CODE HERE
+def print_tuple(tup):
+    for i in tup:
+        print(i)
+
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+lonely_tuple = ("halp")
+print(lonely_tuple)
+
+u = (1, 2)  # Q: What needs to be added to make this work? A: more than one element to iterate over
 print_tuple(u)
