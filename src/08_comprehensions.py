4ac74417c7922ea8ac55d9d61979185b0c5f61ef
kmcknight1 <katiemcknight1991@gmail.com>
2020-04-08 00:52:18 -0500

    refactors comprehension code

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,26 +10,22 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-for x in range(5):
-    y.append(x+1)
+y = [num + 1 for num in range(5)]
+
 print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-for x in range(10):
-    y.append(x**3)
+y = [num**3 for num in range(10)]
+
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
-y = []
-for x in a:
-    y.append(x.upper())
+y = [word.upper() for word in a]
 
 print(y)
 
@@ -39,10 +35,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
-
-for i in x:
-    if int(i) % 2 == 0:
-        y.append(int(i))
+y = [num for num in range(len(x)) if num % 2 == 0]
 
 print(y)
